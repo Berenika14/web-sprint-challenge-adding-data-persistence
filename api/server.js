1d374765d5de794efabcb31ccf3a1e82419fb4b4
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-18 12:18:37 -0500

    Installed the needed dependencies on package.json file , started the server and completed index.js file

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
 // build your server here and require it from index.js
+
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("*", (req, res) => {
+  res.status(200).json("Getting all API 😁");
+});
+
+module.exports = server;
